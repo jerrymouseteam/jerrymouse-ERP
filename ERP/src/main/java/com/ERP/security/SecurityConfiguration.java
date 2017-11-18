7337ea956ed9ac7054ba1e16962472d8f0103580
karan Sutarwala <karan Sutarwala@hell.com>
2017-11-18 17:58:19 +0530

    changes for junior eng

--- ERP/src/main/java/com/ERP/security/SecurityConfiguration.java
+++ ERP/src/main/java/com/ERP/security/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationTrustResolverIm
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,7 +29,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	PersistentTokenRepository tokenRepository;
 
 	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+			throws Exception {
 		auth.userDetailsService(userDetailsService);
 		auth.authenticationProvider(authenticationProvider());
 		// web.ignoring().antMatchers("/forgotPassword");
@@ -38,14 +38,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/list","/service/user/","/listproject")
-				.access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA')")
-				.antMatchers("/newuser/**", "/delete-user-*","/addproject/**").access("hasRole('ADMIN')").antMatchers("/edit-user-*")
-				.access("hasRole('ADMIN') or hasRole('DBA')").and().formLogin().loginPage("/login")
-				.loginProcessingUrl("/login").usernameParameter("ssoId").passwordParameter("password").and()
-				.rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository)
-				.tokenValiditySeconds(86400).and().csrf().and().exceptionHandling().accessDeniedPage("/Access_Denied");
-		
+		http.authorizeRequests()
+				.antMatchers("/", "/list", "/service/user/", "/listproject")
+				.access("hasRole('USER') or hasRole('ADMIN') or hasRole('DBA') or hasRole('Junior Engineer')")
+				.antMatchers("/newuser/**", "/delete-user-*", "/addproject/**")
+				.access("hasRole('ADMIN')")
+				.antMatchers("/edit-user-*")
+				.access("hasRole('ADMIN') or hasRole('DBA') or hasRole('Junior Engineer')")
+				.and().formLogin().loginPage("/login")
+				.loginProcessingUrl("/login").usernameParameter("ssoId")
+				.passwordParameter("password").and().rememberMe()
+				.rememberMeParameter("remember-me")
+				.tokenRepository(tokenRepository).tokenValiditySeconds(86400)
+				.and().csrf().and().exceptionHandling()
+				.accessDeniedPage("/Access_Denied");
+
 	}
 
 	@Bean
@@ -72,8 +79,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public AuthenticationTrustResolver getAuthenticationTrustResolver() {
 		return new AuthenticationTrustResolverImpl();
 	}
-	/*@Override
-	public void configure(WebSecurity web) throws Exception {
-	    web.ignoring().antMatchers("/forgotPassword");
-	}*/
+	/*
+	 * @Override public void configure(WebSecurity web) throws Exception {
+	 * web.ignoring().antMatchers("/forgotPassword"); }
+	 */
 }
